42643e2a5993c10c3969dbcf8e58d9468b4db002
dylanajones <jonesadylan@gmail.com>
2016-11-09 14:48:57 -0800

    Adding in test copde for listener

--- src/wind_converter.py
+++ src/wind_converter.py
@@ -1,9 +1,25 @@
+#!/usr/bin/env python
+
 # Code for converting wind sensor data to world frame
 
 import rospy
 
-class Converter():
+from geographic_msgs.msg import Vector3
+from geographic_msgs.msg import GeoPose
+
+class Converter:
 
 	def __init__(self):
 
-		self.wind_sub = 
+		self.wind_sub = rospy.Subscriber('/crw_wind_pub', Vector3, self.wind_callback)
+		self.pose_sub = rospy.Subscriber('/crw_geopose_pub', GeoPose, self.pose_callback)
+
+		self.wind_pub = rospy.Publisher('/wind_converted', Vector3, queue_size=100)
+
+		self.current_pose = GeoPose()
+
+	def wind_callback(self, msg):
+		print "Wind callback"
+
+	def pose_callback(self, msg):
+		print "Pose callback"
